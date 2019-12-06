e3710b1163cc5a48a1ddce9e8739d6523547e130
suhanna <suhanna52@gmail.com>
2019-12-06 18:56:59 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="system_info", # Replace with your own username
+    version="0.0.1",
+    author="Suhanna CH",
+    author_email="suhanna52@gmail.com",
+    description="A package for accessing system informations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['py-cpuinfo', 'requests'],
+)
