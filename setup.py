a48af5d63538f98a3d5fd98a0e96dbeb058cfd6e
suhanna <suhanna52@gmail.com>
2019-12-06 18:59:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package for accessing system informations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/suhanna/System_info",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
